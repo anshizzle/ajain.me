4aba4e2c0380a7e6709542611a7766e16feb4f0c
Anshul Jain <aj@capyx.com>
2017-02-16 15:21:14 -0800

    added heart egg

--- app/assets/javascripts/easter_eggs.js
+++ app/assets/javascripts/easter_eggs.js
@@ -1,3 +1,33 @@
 cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
   $(".aj-bitmoji").addClass("devil");
+  $("body").addClass('black');
+  let audio = new Audio('assets/evil_laugh');
+  audio.play();
+  $(audio).bind('ended', clearEggs);
 });
+
+
+cheet('shift , 3', function () {
+  $(".aj-bitmoji").addClass("heart");
+  console.log("<3 you");
+
+  setTimeout(clearEggs, 2000)
+});
+
+
+$(".aj-bitmoji").click(function () {
+  $(".aj-bitmoji").addClass("spit");
+  let audio = new Audio('assets/spitting');
+  audio.play();
+  $(audio).bind('ended', clearEggs);
+})
+
+function clearEggs() {
+  $(".aj-bitmoji").attr("class").split(' ').forEach(function(class_name) {
+    if(class_name !== 'aj-bitmoji') {
+      $('.aj-bitmoji').removeClass(class_name);
+    }
+  });
+
+  $('body').removeClass('black');
+}
